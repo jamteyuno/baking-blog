f5769eac1625ba0faeb082f847e4c0748f8f9e8f
Janice Mendoza <janice.mendoza@paymark.co.nz>
2024-06-19 11:29:17 +1200

    Add wishlist

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,21 @@
+export default function Page() {
+return (
+    <section>
+    <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Wishlist</h1>
+    <div>Welcome to the world of a baking wishlist, where every item whispers promises of delectable creations waiting to be made.</div>
+    <article className="prose">
+        <ul>
+            <li>
+                <a href="https://nz.brodandtaylor.com/collections/bread-baking/products/folding-proofer-slow-cooker">Brod and Taylor Proofer</a>
+            </li>
+            <li>
+                <a href="https://nz.brodandtaylor.com/collections/bread-baking/products/sourdough-home">Sally's Home</a>
+            </li>
+            <li>
+                <a href="https://www.zassenhaus-brandshop.com/en/Zassenhaus-Bread-slicing-machine-Classic/40459">Bread Slicing Machine</a>
+            </li>
+        </ul>
+    </article>
+    </section>
+)
+}
